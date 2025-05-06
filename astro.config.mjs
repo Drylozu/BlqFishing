45ece939fdb8e49571ca22bfda8be5e7458d1a94
Drylozu <35281350+Drylozu@users.noreply.github.com>
2025-05-06 00:42:52 -0500

    worker

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   integrations: [AstroPWA({
+    injectRegister: 'script',
     registerType: 'autoUpdate',
   })]
 });
