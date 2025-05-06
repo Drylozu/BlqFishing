273f445b2799fe7184decc87563c1780083af598
Drylozu <35281350+Drylozu@users.noreply.github.com>
2025-05-06 00:29:07 -0500

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import AstroPWA from '@vite-pwa/astro';
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [AstroPWA({
+    registerType: 'autoUpdate',
+  })]
+});
